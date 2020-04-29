6733462b62b3753f58184a1cf88db15b24f334a6
oakkao <57321684+oakkao@users.noreply.github.com>
2020-04-29 12:21:05 +0700

    ใส่ตัวเกมเพิ่มแล้ว

---
+++ Match 3 Clone/Assets/Scenes/MainManuScene/ButtonControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonControl : MonoBehaviour
+{
+    public void MainScene(){
+        SceneManager.LoadScene("mainmanu");
+
+    }
+    public void settingScene(){
+        SceneManager.LoadScene("setting");
+
+    }
+    public void minigameScene(){
+        SceneManager.LoadScene("minigame");
+
+    }
+    public void workshopScene(){
+        SceneManager.LoadScene("workshop");
+
+    }
+
+}
---
+++ Match 3 Clone/Assets/Scenes/SettingScene/EffectSet.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectSet : MonoBehaviour
+{
+    public GameObject indicator;
+    public bool effect_on;
+    public float movespeed;
+    private float t = 0.0f;
+    private bool switching =false;
+    private float onX = 18.5f;
+    private float offX = -18.5f;
+    private float Actx;
+    private RectTransform toggle;
+    private RectTransform handle;
+
+    public void Awake(){
+            toggle = indicator.GetComponent<RectTransform>();
+    }
+
+    public void ToggleEffect(){
+     //   Debug.Log("Debugg");
+        if(effect_on){
+            effect_on=false;
+            switching = true;
+        }else {
+            effect_on=true;
+            switching = true;
+        }
+        
+
+    }
+    void Update(){
+  //      Actx=toggle.sizeDelta.x;
+        if(switching){
+            if(effect_on){
+                toggle.localPosition = MoveIndicator(indicator,offX,onX);
+            }else{
+                toggle.localPosition = MoveIndicator(indicator,onX,offX);
+            }
+            
+        }
+       // Debug.Log("Say Hi");
+    }
+    private Vector3 MoveIndicator(GameObject movehand,float startX,float endX){
+        Vector3 position = new Vector3(Mathf.Lerp(startX,endX,t+=movespeed*Time.deltaTime),0,0);
+        if(t>1){
+            t=0;
+            switching = false;
+
+        }
+        return position;
+
+    }
+}
