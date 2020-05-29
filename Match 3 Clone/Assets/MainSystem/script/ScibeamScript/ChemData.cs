d89346fe97b75c1a3dc7bea64bbfb38318b0247c
oakkao <57321684+oakkao@users.noreply.github.com>
2020-05-29 16:04:42 +0700

    แปะ

---
+++ Match 3 Clone/Assets/MainSystem/script/ScibeamScript/ChemData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Chem_Data", menuName = "Problem/ChemData")]
+public class ChemData : ScriptableObject
+{
+    [SerializeField] public List<ChemEq> chemdata = new List<ChemEq>();
+}
+[System.Serializable]
+public class ChemEq{
+    [SerializeField] public List<slot> input = new List<slot>();
+    [SerializeField] public List<slot> output = new List<slot>();
+}
