a6e4223c51c910d06981c843d851dc0f8f4f2dd1
oakkao <57321684+oakkao@users.noreply.github.com>
2020-09-01 23:12:46 +0700

    หน้าต่างใส่สารขยายตามช่องเก็บของ

--- Match 3 Clone/Assets/Scenes/workshop/InputWindowScript.cs
+++ Match 3 Clone/Assets/Scenes/workshop/InputWindowScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class InputWindowScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject inventory;
+    public GameObject button;
+    RectTransform window;
+    float height;
+    float sty;
+    float stx;
+    float bty;
+    RectTransform rt;
+    RectTransform bt;
     void Start()
     {
-        
+      //  window = GetComponent<RectTransform>();
+      rt = GetComponent<RectTransform>();
+      bt = button.GetComponent<RectTransform>();
+      height = rt.rect.height;
+      sty = rt.localPosition.y;
+      stx = rt.localPosition.x;
+      bty = bt.localPosition.y;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        float temp = inventory.GetComponent<RectTransform>().offsetMax.y;
+        temp = 400-temp;
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x,height + temp);
+        rt.localPosition = new Vector2(stx,sty - temp/2);
+        bt.localPosition = new Vector2(bt.localPosition.x,bty + temp/2);
     }
 }
