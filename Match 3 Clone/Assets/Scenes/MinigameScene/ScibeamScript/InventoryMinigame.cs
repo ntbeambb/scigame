35c953a8ab505afcbc4da09800434d5b4bfdcad2
oakkao <57321684+oakkao@users.noreply.github.com>
2020-10-14 09:59:29 +0700

    ปรับขนาดของใน minigame

--- Match 3 Clone/Assets/Scenes/MinigameScene/ScibeamScript/InventoryMinigame.cs
+++ Match 3 Clone/Assets/Scenes/MinigameScene/ScibeamScript/InventoryMinigame.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class InventoryMinigame : MonoBehaviour
 {
     private int nextpo;
     public InventorySystem backpack;
-    public List<GameObject> ItemList = new List<GameObject>();
+    //public List<GameObject> ItemList = new List<GameObject>();
     public int Slot;
     public int StartPo;
+    public AllpicID allpic;
     private GameObject[] DisplaySlot;
     private GameObject[] NumSlot;
     private Vector2 BarPo;
     public float StartX;
     private TextMeshPro textmesh;
+    public GameObject gob;
 
     public GameObject ShowNum;
+    public float Itemsize;
     void Awake(){
         StartPo = 0;
         DisplaySlot = new GameObject[Slot];
@@ -41,18 +45,32 @@ public class InventoryMinigame : MonoBehaviour
     }
 
 
-    private GameObject AddInventory(int po,GameObject gob)
+    private GameObject AddInventory(int po,int _id)
     {
+        //set start position
         Vector2 tilepo = new Vector2(transform.position.x + po*1.82f - StartX,transform.position.y);
+        //create gameobject
 		GameObject tile = Instantiate(gob,tilepo, Quaternion.identity) as GameObject;
-        tile.transform.parent = this.transform;
+        //insert image
+        tile.GetComponent<Image>().sprite = allpic.ID[_id];
+        //insert scibaem data
+        tile.GetComponent<ScibeamData>().id = _id;
+        //resize
+        var sx = allpic.ID[_id].bounds.size.x / Itemsize;
+        var sy = allpic.ID[_id].bounds.size.y / Itemsize;
+        tile.GetComponent<RectTransform>().localScale = new Vector2(sx,sy);
+        //relocate
+        var pox = tile.GetComponent<RectTransform>().position;
+        pox.x += (sx-1f)/2;
+        tile.GetComponent<RectTransform>().position = pox;
+        //set parent
+        tile.transform.SetParent(transform);
+        //rename
         tile.name = "Inventory "+po;
         return tile;
     }
     private GameObject AddNumber(int num){
-        int ck = 0;
-        if(num>9)ck++;
-        if(num>99)ck++;
+        int ck = (int)Mathf.Log10(num);
         Vector2 tilepo = new Vector2(transform.position.x + 1.1f - 0.6f*ck,transform.position.y);
 		GameObject NumDis = Instantiate(ShowNum,tilepo, Quaternion.identity,this.transform) as GameObject;
         textmesh = NumDis.GetComponent<TextMeshPro>();
@@ -61,7 +79,7 @@ public class InventoryMinigame : MonoBehaviour
         NumDis.name = "Item Amount";
         return NumDis;
     }
-    private GameObject findgameobject(int _id){
+    /*private GameObject findgameobject(int _id){
         int co = ItemList.Count;
         int i;
         for(i=0;i<co;i++){
@@ -70,7 +88,7 @@ public class InventoryMinigame : MonoBehaviour
             }
         }
         return null;
-    }
+    }*/
     public void display(){
         //Debug.Log("Display");
         int count = backpack.container.Count;
@@ -83,10 +101,10 @@ public class InventoryMinigame : MonoBehaviour
         for(int i=0; i < Slot && i+StartPo < count; i++){
             
             //Debug.Log("BUG "+i);
-            GameObject tempgameobject = findgameobject(backpack.container[i+StartPo].id);
+            //GameObject tempgameobject = findgameobject(backpack.container[i+StartPo].id);
             //if(tempgameobject == null)Debug.Log("Bug Null");
             int num=backpack.container[i+StartPo].amount;
-            DisplaySlot[i] = AddInventory(i,tempgameobject);
+            DisplaySlot[i] = AddInventory(i,backpack.container[i+StartPo].id);
             //Show Volume of Item
             NumSlot[i] = AddNumber(num);
             //float zz = DisplaySlot[i].GetComponent<Transform>().position.x;
