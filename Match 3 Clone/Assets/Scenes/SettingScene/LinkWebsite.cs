7d3ffb57b71a5f9cb0df90c4ed10b341a544b151
oakkao <57321684+oakkao@users.noreply.github.com>
2021-01-21 23:25:11 +0700

    hotfix เถื่อน1

---
+++ Match 3 Clone/Assets/Scenes/SettingScene/LinkWebsite.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkWebsite : MonoBehaviour
+{
+    public void OpenURL()
+    {
+        Application.OpenURL("https://play.google.com/store/apps/details?id=com.SciBEAM.SciBEAM");
+    }
+}
